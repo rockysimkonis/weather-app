3b1fbb041fb565aac7be0b7a5a7d81c8e2ecc4dd
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-07-06 12:07:25 -0500

    Building weather api request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = 'fbf63c7c12a36fdf2f7d4e0fedc1d956';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
