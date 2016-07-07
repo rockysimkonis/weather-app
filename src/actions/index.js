be3351dedb49ed59c4986cbec1702df9641e9186
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-07-07 17:41:32 -0500

    Added live google map

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'fbf63c7c12a36fdf2f7d4e0fedc1d956';
+const API_KEY = 'c779bd340f707bf8cb5cf4b6c52bfab3';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
